576ec18df656d098807eb4a0ad493d24118e591b
unspecifiedhuman <57191192+unspecifiedhuman@users.noreply.github.com>
2022-02-08 23:50:44 +0000

    Update nhentai-gen-tag.py

--- nhentai-gen-tag.py
+++ nhentai-gen-tag.py
@@ -2,19 +2,20 @@ import requests, re
 a=''
 name=input('name to save file as: ')
 per='tag'
-v=input('input 1 if searching for character, leave blank if searching for a tag: ')
-tag=input('tag/char: ')
+v=input('tags or character seperated by a comma: ') #e.g. rem,ram,anal
+v=v.replace(',','+')
+b=v
 number=int(input('no. of pages: '))
-z=tag
-if v == '1':
-        per='character'
 for i in range (0,number):
-        req=requests.get('https://nhentai.net/' + per + '/' + tag)
+        req=requests.get('https://nhentai.net/search/?q=' + v)
         for m in re.finditer('href=', req.text):
                 h=m.end()+11
                 a=req.text[m.start():h]
                 if '/g/' in a:
+                        a=a.replace('href="','nhentai.net')
+                        while a[-1] != '/':
+                                a=a[:-1]
                         print(a)
                         with open(name + ".txt", "a") as f:
                                 f.write('\n' + a)
-        tag = z + '/?page=' + str(i)
+        v = b + '&page=' + str(i)

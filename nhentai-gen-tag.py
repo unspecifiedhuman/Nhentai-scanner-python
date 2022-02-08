d9a6bc4b9d416f32ac6becfeb6e6a1172ffcb486
unspecifiedhuman <57191192+unspecifiedhuman@users.noreply.github.com>
2022-02-08 22:44:53 +0000

    Update nhentai-gen-tag.py

--- nhentai-gen-tag.py
+++ nhentai-gen-tag.py
@@ -1,6 +1,6 @@
+GNU nano 6.0        nhentai-gen-tag.py
 import requests, re
 a=''
-no=0
 name=input('name to save file as: ')
 per='tag'
 v=input('input 1 if searching for character, leave blank if searching for a tag: ')
@@ -8,15 +8,14 @@ tag=input('tag/char: ')
 number=int(input('no. of pages: '))
 z=tag
 if v == '1':
-	per='character'
-while no != number:
-	req=requests.get('https://nhentai.net/' + per + '/' + tag)
-	for m in re.finditer('href=', req.text):
-		h=m.end()+11
-		a=req.text[m.start():h]
-		if '/g/' in a:
-			print(a)
-			with open(name + ".txt", "a") as f:
-				f.write('\n' + a)
-	no=no+1
-	tag = z + '/?page=' + str(no+1)
+        per='character'
+for i in range (0,number):
+        req=requests.get('https://nhentai.net/' + per + '/' + tag)
+        for m in re.finditer('href=', req.text):
+                h=m.end()+11
+                a=req.text[m.start():h]
+                if '/g/' in a:
+                        print(a)
+                        with open(name + ".txt", "a") as f:
+                                f.write('\n' + a)
+        tag = z + '/?page=' + str(i)

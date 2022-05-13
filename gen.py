e3dd8a8fb586255b194001b4e4ba6071ef5795b4
unspecifiedhuman <57191192+unspecifiedhuman@users.noreply.github.com>
2022-05-13 22:43:36 +0100

    Add files via upload

--- gen.py
+++ gen.py
@@ -1,14 +1,36 @@
-import requests
-no=int(input('number of urls: '))
+import requests, re, wget, os, time
+no=int(input('number of galleries: '))
 b=''
-name=input('name: ')
-while True:
-        for i in range (0,100):
-                h=requests.get('https://nhentai.net/random')
-                while h.url == 'https://nhentai.net/random/':
-                        h=requests.get('https://nhentai.net/random')
-                b = b + "\n" + h.url
-                print(i)
-        with open(name + ".txt", "a") as f:
-                f.write('\n' + b)
-                b=''
+u=0
+for i in range (0,no):
+	h=requests.get('https://nhentai.net/random')
+	while h.url == 'https://nhentai.net/random/':
+		h=requests.get('https://nhentai.net/random')
+	b =  "\n" + h.url
+	print(b)
+	for g in re.finditer('"name" content=', h.text):
+		y=g.end()+80
+		j=h.text[g.start()+16:y]
+		while j[-20:] != '" /><meta itemprop="':
+			j=j[:-1]
+		j=j[:-20]
+		j=j.replace(' ', '_')
+		print(j)
+		os.mkdir('files/' + j)
+	for m in re.finditer('data-src=', h.text):
+		u+=1
+		l=m.end()+55
+		z=h.text[m.start():l]
+		while z[-1] != 'g':
+			z=z[:-1]
+		z=z[10:]
+		print(str(z))
+		r=requests.get(str(z))
+		open(str(u) + str(z[-4:]), 'wb').write(r.content)
+		os.system('mv ' + str(u) + str(z[-4:]) + ' files/"' + j + '"/')
+
+		q="'"
+#		os.system("wget -o " + str(u) + str(z[-4:]) + " " +  str(z))
+		r='<p><img src=' + q + './' + str(u) +  str(z[-4:]) + '' + q + '></p>'
+		os.system('echo "' + str(r) + '" >> ~/bot/files/' + str(j) +  '/index.html')
+#		os.system('rm ' + str(u) + str(z[-4:]))

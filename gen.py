a96607fef84c9a01f6df3c8da86d4edbc0169932
unspecifiedhuman <57191192+unspecifiedhuman@users.noreply.github.com>
2022-05-15 01:22:00 +0100

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,8 +1,6 @@
 import requests
 no=int(input('number of urls: '))
 na=input('name of file:')
-b=''
-u=0
 for i in range (0,no):
 	h=requests.get('https://nhentai.net/random')
 	while h.url == 'https://nhentai.net/random/':
